b59eb72fcf6e9f3ac4decd75d9c9c712db0fc1ef
Jeremy Pena <5196790+jerpena@users.noreply.github.com>
2022-06-05 17:18:20 -0500

    update errorHandler

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -7,8 +7,6 @@ export const errorHandler = (err, req, res, next) => {
 
     res.json({
         message: err.message,
-        stack: process.env.NODE_ENV === 'production'
-            ? null
-            : err.stack
+        ...(process.env.NODE_ENV !== 'production' && { stack: err.stack })
     });
 };
